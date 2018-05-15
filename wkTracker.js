f2127d6715d0f3ac9230eb38cb644f35f77a81de
Alicia Boya García <ntrrgc@gmail.com>
2018-05-15 19:06:54 +0200

    Simplify interval/timeout code

--- wkTracker.js
+++ wkTracker.js
@@ -86,34 +86,21 @@ var WKTracker = new Lang.Class({
         this.enabled = false; // whether requests will be sent
         this.announceFn = blackHoleAnnounceFn;
         this.httpSession = new Soup.SessionAsync();
-        this.orderedOperatorServerReviewData = new OrderedRequestResponseOperator();
         this.switchLatestTimedReviewData = new SwitchLatestOperator();
     },
 
     enableUpdates(announceFn) {
         this.announceFn = announceFn;
-        this.announceFn({text: "WK 待ってください", reviewsAvailable: null});
 
         // Enable polling of the review queue
         this.enabled = true;
 
         // Make the first request, but not immediately, wait 1 second before just in case the extension
         // gets immediately disabled (which happens sometimes during GNOME start-up for unknown reasons)
-        Mainloop.timeout_add_seconds(1, this.orderedOperatorServerReviewData.wrapResponseCallback(() => {
-            if (this.enabled) {
-                this._makeUpdateRequest();
-            }
-            return false; // don't repeat this timer
-        }));
-
         const pollingIntervalSeconds = 10 * 60;
-        Mainloop.timeout_add_seconds(pollingIntervalSeconds, this.orderedOperatorServerReviewData.wrapResponseCallback(() => {
-            // The extension may have been disabled since the task was enqueued.
-            if (this.enabled) {
-                this._makeUpdateRequest();
-            }
-            return true;
-        }));
+        this._repeatWhileEnabled(1, pollingIntervalSeconds, () => this._makeUpdateRequest());
+
+        this._repeatWhileEnabled(0, 50, () => this._updateUiTime());
     },
 
     disableUpdates() {
@@ -121,29 +108,46 @@ var WKTracker = new Lang.Class({
         this.enabled = false;
     },
 
+    _repeatWhileEnabled(timeoutFirstCall, interval, task) {
+        Mainloop.timeout_add_seconds(timeoutFirstCall, () => {
+            if (this.enabled) {
+                task();
+
+                Mainloop.timeout_add_seconds(interval, () => {
+                    if (this.enabled) {
+                        task();
+                        return true; // repeat
+                    } else {
+                        return false; // not anymore
+                    }
+                });
+            }
+            return false; // just once
+        });
+    },
+
     _makeUpdateRequest() {
-        // realUrl = `https://www.wanikani.com/api/user/${wkApiKey}/study-queue`
         const request = Soup.Message.new("GET", `https://www.wanikani.com/api/user/${wkApiKey}/study-queue`);
         this.httpSession.queue_message(request, (httpSession, message) => {
             const responseText = message.response_body.flatten().get_data().toString();
-            // print(responseText)
+            print(`Request response: ${responseText}`);
 
             try {
                 const parsedResponse = JSON.parse(responseText);
-                this._receivedUpdateResponse(parsedResponse);
+                this.latestReviewInfo = parsedResponse;
+                this._updateUiTime();
             } catch (err) {
                 log(`Failed to parse server response: ${responseText}`);
             }
         });
     },
 
-    _receivedUpdateResponse(parsedResponse) {
-        print(JSON.stringify(parsedResponse));
-        this.latestReviewInfo = parsedResponse;
-        this._newServerReviewInfo();
-    },
+    _updateUiTime() {
+        if (!this.latestReviewInfo) {
+            this.announceFn({text: "WK 待ってください", reviewsAvailable: null});
+            return;
+        }
 
-    _newServerReviewInfo() {
         const reviewsAvailable = this.latestReviewInfo.requested_information.reviews_available;
         const timeToNextReviewFullMs = Math.max(0,
             new Date(this.latestReviewInfo.requested_information.next_review_date * 1000).getTime() -
@@ -157,32 +161,14 @@ var WKTracker = new Lang.Class({
         const seconds = Math.floor(remainderTotalSeconds);
 
         let text;
-        if (reviewsAvailable > 0) {
-            text = `WK ${reviewsAvailable}枚`
-        } else if (timeToNextReviewFullMs > 0) {
+        if (timeToNextReviewFullMs > 0) {
             text = `WK ${hours}h${pad2(minutes)}m`
         } else {
-            text = 'WK Vacation mode'
+            text = `WK ${reviewsAvailable}枚`
         }
-        const announcement = {
+        this.announceFn({
             text: text,
             reviewsAvailable: reviewsAvailable,
-        };
-
-        this.announceFn(announcement);
-        if (reviewsAvailable === 0 && timeToNextReviewFullMs > 0) {
-            // When the current minute in the countdown ends, emit a new announcement.
-
-            // We wait `seconds + 2`. One extra second is to account for the truncation
-            // made when calculating `seconds`. The other one is to avoid hitting exactly
-            // seconds = 0, which belongs to the same minute and therefore would not
-            // actually require an update.
-            const secondsToNextUpdate = seconds + 2;
-            Mainloop.timeout_add_seconds(secondsToNextUpdate,
-                this.switchLatestTimedReviewData.wrap(() => {
-                    this._newServerReviewInfo();
-                    return false; // don't let glib repeat this callback by itself
-                }));
-        }
+        });
     }
 });
